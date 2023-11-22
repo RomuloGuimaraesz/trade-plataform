ef8c3db8e27c2dc5969811276725495da50ca3cd
Rômulo de farias <romulo.fantomaz@gmail.com>
2023-11-22 20:31:48 -0300

    Updates the main.js and style.css with new markup and css animations

--- src/main.js
+++ src/main.js
@@ -1,20 +1,24 @@
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <section>
-      <header>
-        <section>
-          <div></div>
-          <div>Home</div>
-          <div>Trade</div>
-        </section>
+    <header>
+      <section>
+        <div></div>
+        <div>Home</div>
+        <div>Trade</div>
+      </section>
 
-        <section>
-          <div></div>
-          <div></div>
-        </section>
-      </header>
-    </section>
-  </div>
+      <section>
+        <div></div>
+        <div></div>
+      </section>
+    </header>
+
+    <main>
+        <h1>Trade <br> Dashboard</h1>
+        <div>
+          <input type="checkbox" id="switch" />
+          <label for="switch">Toggle</label>
+        </div>
+    </main>
 `
